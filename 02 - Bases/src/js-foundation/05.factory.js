e38cce8b52a8a6b9c02db656d03d3bc460db5a62
Estela Marchesani <estelamarchesani@gmail.com>
2026-03-31 01:23:37 -0300

    added factory function not tightly coupled

--- 02 - Bases/src/js-foundation/05.factory.js
+++ 02 - Bases/src/js-foundation/05.factory.js
@@ -1,3 +1,5 @@
+const { v4: uuidv4 } = require('uuid');
+const getAge = require('get-age')
 
 //Factory Function fuertemente acoplada: 
 const buildPerson = ({name, birthdate}) => {
@@ -5,10 +7,12 @@ const buildPerson = ({name, birthdate}) => {
 Objeto construido con la función buildPerson: 
         `);
     return {
-        id: new Date().getTime(), 
+        id: uuidv4(),
+        //id: new Date().getTime(), 
         name: name,
         birthdate: birthdate,
-        age: new Date().getFullYear() - new Date(birthdate).getFullYear()
+        agre:getAge(birthdate) //otra forma de calcular la edad, utilizando la libreria get-age, que se instala con npm install get-age, y se importa al inicio del archivo, y se le pasa el birthdate para que calcule la edad, y se devuelve el resultado de getAge(birthdate) como el valor de age, en lugar de calcularlo manualmente con new Date().getFullYear() - new Date(birthdate).getFullYear()
+        //age: new Date().getFullYear() - new Date(birthdate).getFullYear()
     }
 }
 
