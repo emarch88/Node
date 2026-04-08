b9c8502fe317bfbc1b83ce1fb3b22f3bf1b8ab09
Estela Marchesani <estelamarchesani@gmail.com>
2026-04-08 01:45:15 -0300

    add comments to consider once Axios is installed

--- 02 - Bases/src/plugins/http-client.plugin.js
+++ 02 - Bases/src/plugins/http-client.plugin.js
@@ -1,11 +1,17 @@
 //Por acá pasan todas las peticiones http:
 
+//If Axios is installed with npm i axios, we can use it as follows:
+//const axios = require('axios'); // legacy way
+
 const httpClientPlugin ={
     get: async (url) => {
         const resp = await fetch(url);
         return await resp.json();
     },
-
+    //const {data} = await axios.get(url); //If Axios is installed
+    //console.log(data); //If Axios is installed
+    //return data; //If Axios is installed
+    
     post: async (url, body) => {},
     put: async (url, body) => {},
     delete: async (url) => {},
